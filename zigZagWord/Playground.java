d47446a05beb22ef55556e69d20a271a8f6be4d7
Parker Bronson <parbro3@gmail.com>
2019-11-08 10:56:55 -0700

    Better runtime and way better memory

--- zigZagWord/Playground.java
+++ zigZagWord/Playground.java
@@ -3,11 +3,50 @@ import java.util.*;
 public class Playground {
 
     public void run(){
-        String asdf = "AB";
-        convert(asdf, 1);
+        String asdf = "PAYPALISHIRING";
+        convert(asdf, 3);
     }
 
-    public String convert(String s, int numRows) {
+    public String convert(String s, int numRows){
+        List<StringBuilder> allSBs = initializeAllSbs(numRows);
+        char[] charArray = s.toCharArray();
+        int row = 0;
+        boolean levelGoingUp = false;
+
+        if(numRows == 1)
+            return s;
+        
+        for(int i = 0; i < charArray.length; i++){
+            StringBuilder sb = allSBs.get(row);
+            sb.append(charArray[i]);
+
+            if(row == numRows - 1)
+                levelGoingUp = true;
+            else if(row == 0)
+                levelGoingUp = false;
+
+            if(!levelGoingUp)
+                row++;
+            else{
+                row--;
+            }
+        } 
+
+        StringBuilder convertedString = new StringBuilder();
+        allSBs.forEach((sb) -> convertedString.append(sb.toString()));
+        return convertedString.toString();
+    }
+
+    public List<StringBuilder> initializeAllSbs(int numRows){
+        List<StringBuilder> allSbs = new ArrayList<StringBuilder>();
+        for(int i = 0; i < numRows; i++){
+            StringBuilder tempSB = new StringBuilder();
+            allSbs.add(tempSB);
+        }
+        return allSbs;
+    }
+
+    public String convertBruteForce(String s, int numRows) {
                
         char[][] completedArray = new char[numRows][s.length()];
         boolean levelGoingUp = false;
