386a9c1c502c0b10b0ac0d8ebd946ecf07d81a47
Parker Bronson <parbro3@gmail.com>
2019-11-22 15:44:48 -0700

    Wow I managed to make it even slower

--- findTheDifference/Playground.java
+++ findTheDifference/Playground.java
@@ -3,18 +3,23 @@ import java.util.*;
 public class Playground {
 
     public void run(){
-        findTheDifference("abcd", "abcde");
+        findTheDifference("a", "aa");
     }
 
     public char findTheDifference(String s, String t) {
 
         Map<Character,Integer> sLetters = populateMap(s);
-        Map<Character,Integer> tLetters = populateMap(t);
+        char[] tLetters = t.toCharArray();
 
         //loop through one map and check the other one as you go
-        for(char character : tLetters.keySet()){
-            if(sLetters.get(character) == null || !(sLetters.get(character) == tLetters.get(character)))
+        for(char character : tLetters){
+            //check the s map with the t letters. If you find it. Decrement.
+            if(sLetters.get(character) != null && sLetters.get(character) > 0){
+                sLetters.put(character, sLetters.get(character) - 1);
+            } else {
                 return character;
+            }
+
         }
         return 0;
     }
@@ -27,7 +32,7 @@ public class Playground {
         for(int i = 0; i < sCharArray.length; i++){
             Integer count = countLetters.get(sCharArray[i]);
             if(count == null)
-                countLetters.put(sCharArray[i], 0);
+                countLetters.put(sCharArray[i], 1);
             else
                 countLetters.put(sCharArray[i], ++count);
         }
