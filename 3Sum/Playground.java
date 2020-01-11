9bc2e5941fed35c3fd3d3040c6ad09ac14b3d162
Parker Bronson <parbro3@gmail.com>
2020-01-11 11:16:12 -0700

    Still can't pass the 311/313 test case

--- 3Sum/Playground.java
+++ 3Sum/Playground.java
@@ -34,8 +34,8 @@ public class Playground {
                 if(i < j){
                     Integer index = crazyMap.get((nums[i]+nums[j])*-1);
                     if(index != null && j < index){
-                        List<Integer> tempList = new ArrayList<>();
-                        tempList.add(nums[i]); tempList.add(nums[j]); tempList.add(nums[index]);
+
+                        List<Integer> tempList = addInOrder(nums[i], nums[j], nums[index]);
 
                         if(!checkAlreadyIn(allLists, tempList))
                             allLists.add(tempList);
@@ -45,6 +45,24 @@ public class Playground {
         }
 
         return allLists;
+    }
+
+    public List<Integer> addInOrder(int i, int j, int k){
+        List<Integer> finalList = new ArrayList<>();
+        finalList.add(i);
+        if(i <= j)
+            finalList.add(j);
+        else
+            finalList.add(0,j);
+        
+        if(k >= i && k >= j){
+            finalList.add(k);
+        } else if(k <= i && k <= j){
+            finalList.add(0,k);
+        } else if((i >= k && j <= k) || (i <= k && j >= k)){
+            finalList.add(1,k);
+        }
+        return finalList;
     }
 
     public boolean checkAlreadyIn(List<List<Integer>> allLists, List<Integer> checkList){
@@ -58,10 +76,7 @@ public class Playground {
     }
 
     public boolean checkSame(List<Integer> list1, List<Integer> list2){
-        for(Integer num : list1){
-            list2.remove(num);
-        }
-        return list2.size() == 0;
+        return list1.get(0) == list2.get(0) && list1.get(1) == list2.get(1) && list1.get(2) == list2.get(2);
     }
 
 }
